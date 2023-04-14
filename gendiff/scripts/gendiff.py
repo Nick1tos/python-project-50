d7a8a1b805f536ca7cb9420740b702b1457bda86
Nick1tos <angeloff555@mail.ru>
2023-04-14 20:23:29 +0300

    delete gendiff

--- gendiff/scripts/gendiff.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-
-'''def parse_args():'''
-parser = argparse.ArgumentParser(prog='gendiff', usage='%(prog)s [-h] first_file second_file', description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.print_help()
